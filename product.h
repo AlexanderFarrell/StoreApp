c4d4d7eb999104d40c93e5432a8513f41ea874e9
Dan Meints <dan-3@hotmail.com>
2022-08-08 21:27:58 -0700

    added product fields to class.

--- product.h
+++ product.h
@@ -1,7 +1,16 @@
 #ifndef STOREAPP_PRODUCT
 #define STOREAPP_PRODUCT
 
+#include <string>
+#include <iostream>
+#include <fstream>
+
 class Product {
+    std::string name;
+    double price;
+    double size;
+    int quantity;
+    
 
 };
 
