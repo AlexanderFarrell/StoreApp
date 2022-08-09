af0d0f54e1921f91bc5b8e81593d5501e31eecbb
Alexander Farrell <alexander.j.farrell@outlook.com>
2022-08-08 22:16:43 -0700

    Implemented Constructor, Prompt, Save and Load methods for the product class.;

--- product.cpp
+++ product.cpp
@@ -1,7 +1,10 @@
 #include "product.h"
 
 Product::Product(std::string name, double price, double size, int quantity) {
-
+    this->name = name;
+    this->price = price;
+    this->size = size;
+    this->quantity = quantity;
 }
 
 Product::~Product() {
@@ -9,11 +12,23 @@ Product::~Product() {
 }
 
 void Product::save(std::string file_name) {
-
+    std::ofstream of(file_name);
+    of << this->name << "\n";
+    of << this->price << "\n";
+    of << this->size << "\n";
+    of << this->quantity << "\n";
+    of.close();
+    std::cout << "Saved new product " << this->name;
+    std::cout << " as " << file_name;
 }
 
 Product::Product(std::string filename) {
-
+    std::ifstream i(filename);
+    i >> this->name;
+    i >> this->price;
+    i >> this->size;
+    i >> this->quantity;
+    i.close();
 }
 
 void Product::display() {
@@ -21,9 +36,27 @@ void Product::display() {
 }
 
 Product Product::prompt() {
+    std::string name;
+    double price;
+    double size;
+    int quantity;
 
+    std::cout << "Enter a name for the new product: ";
+    std::cin >> name;
+    
+    std::cout << "Enter a price: $";
+    std::cin >> price;
+
+    std::cout << "Enter a size: ";
+    std::cin >> size;
+
+    std::cout << "Enter a quantity: ";
+    std::cin >> quantity;
+
+    Product retVal(name, price, size, quantity);
+    return retVal;
 }
 
 std::string Product::get_name() {
-
+    return this->name;
 }
