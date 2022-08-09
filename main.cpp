6a82a6412f705a565f09f4df933d7fb29978da87
Alexander Farrell <alexander.j.farrell@outlook.com>
2022-08-08 21:40:50 -0700

    Implemented the main menu screen in the main function.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 
+void view_product() {
+    
+}
+
+void create_product() {
+
+}
+
+void list_products() {
+
+} 
+
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello world\n";
+    std::cout << "Welcome\n";
+    
+    bool running = true;
+    while (running)
+    {
+        std::cout << "Please enter an option\n";
+        std::cout << " 1. List Products\n";
+        std::cout << " 2. Create Product\n";
+        std::cout << " 3. View Product\n";
+        std::cout << " 4. Quit\n";
+        std::cout << " > ";
+
+        int selection;
+        std::cin >> selection;
+
+        switch (selection) {
+            case 1: 
+                list_products();
+                break;
+            case 2: 
+                create_product();
+                break;
+            case 3: 
+                view_product();
+                break;
+            case 4: 
+                running = false;
+                std::cout << "Quitting... Thank you.";
+                break;
+            default:
+                std::cout << "Invalid entry.";
+                std::cout << "Please enter a number between 1 and 4";
+        }
+    }
+    
+
+
+
+
+
     return 0;
 }
